a4b02063791a1abf02445a5f9bbf6968b7a3bd8b
AbubakarCinnova <amahboob@cinnova.com>
2023-05-26 15:20:26 +0500

    add logo in the footer

--- footer.php
+++ footer.php
@@ -2,12 +2,13 @@
 
 <section class="full-row call text-center text-white">
 
-    <div class="container">
+        <div class="container">
 
-        <h2 style="color: white;">Please call us at <a href="tel:7409294328" style="color: #bebebe;">(740) 929-4328</a>
-            To Make an Appointment.</h2>
+                <h2 style="color: white;">Please call us at <a href="tel:7409294328" style="color: #bebebe;">(740)
+                                929-4328</a>
+                        To Make an Appointment.</h2>
 
-    </div>
+        </div>
 
 
 
@@ -16,126 +17,109 @@
 
 <footer class="full-row">
 
-    <footer class="full-row main-footer text-white">
-        <div class="container">
-            <div class="col-md-5 col-sm-12 col-xs-12 footer-item">
+        <footer class="full-row main-footer text-white">
+                <div class="container">
+                        <div class="col-md-5 col-sm-12 col-xs-12 footer-item">
 
-                <h3>About</h3>
-                <p><img src="<?php echo $base_url ?>/img/logo-footer.png"></p>
-                <p class="light">At Performance Heating and Cooling Services, We service, repair, sell and install
-                    Residential and Commercial Heating and Cooling Systems throughout Licking, North Fairfield, and
-                    Franklin Counties of Ohio. With our highly trained, friendly and experienced technicians, we
-                    guarantee a job done right, with quality you can trust for years to come.</p>
+                                <h3>About</h3>
+                                <p><img src="<?php echo $base_url ?>/img/logo-footer.png"> <img src="<?php echo $base_url ?>/img/premier dealer black.png" alt=""></p>
+                                <p class="light">At Performance Heating and Cooling Services, We service, repair, sell
+                                        and install
+                                        Residential and Commercial Heating and Cooling Systems throughout Licking, North
+                                        Fairfield, and
+                                        Franklin Counties of Ohio. With our highly trained, friendly and experienced
+                                        technicians, we
+                                        guarantee a job done right, with quality you can trust for years to come.</p>
 
-            </div>
-            <div class="col-md-3 col-sm-12 col-xs-12 footer-item">
-                <h3>Service Areas</h3>
+                        </div>
+                        <div class="col-md-3 col-sm-12 col-xs-12 footer-item">
+                                <h3>Service Areas</h3>
 
-                <ul class="footer-list">
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Heath-Ohio-heating-and-air-conditioning-repair.php">Heath</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Newark-Ohio-heating-and-air-conditioning-repair.php">Newark</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Granville-heating-and-air-conditioning-repair.php">Granville</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Hebron-Ohio-heating-and-air-conditioning-repair.php">Hebron</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Buckeye-Lake-heating-and-air-conditioning-repair.php">Buckeye
-                            Lake</a> </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Thornville-Ohio-heating-and-air-conditioning-repair.php">Thornville</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Millersport-heating-and-air-conditioning-repair.php">Millersport</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Pataskala-Ohio-heating-and-air-conditioning-repair.php">Pataskala</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Alexandria-Ohio-heating-and-air-conditioning-repair.php">Alexandria</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Johnstown-Ohio-heating-and-air-conditioning-repair.php">Johnstown</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Glenford-Ohio-heating-and-air-conditioning-repair.php">Glenford</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Kirkersville-Ohio-heating-and-air-conditioning-repair.php">Kirkersville</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Etna-Ohio-heating-and-air-conditioning-repair.php">Etna</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Jacksontown-Ohio-heating-and-air-conditioning-repair.php">Jacksontown
-                        </a> </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Baltimore-Ohio-heating-and-air-conditioning-repair.php">Baltimore</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Reynoldsburg-Ohio-heating-and-air-conditioning-repair.php">Reynoldsburg</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Canal-Winchester-Ohio-heating-and-air-conditioning-repair.php">Canal
-                            Winchester</a> </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/New-Albany-Ohio-heating-and-air-conditioning-repair.php">New
-                            Albany</a> </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Pickerington-Ohio-heating-and-air-conditioning-repair.php">Pickerington</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Jersey-Ohio-heating-and-air-conditioning-repair.php">Jersey</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Gratiot-Ohio-heating-and-air-conditioning-repair.php">Gratiot</a>
-                    </li>
-                    <li><a
-                            href="<?php echo $base_url ?>/service-area/Pleasantville-Ohio-heating-and-air-conditioning-repair.php">Pleasantville</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-4 col-sm-12 col-xs-12 footer-item">
-                <h3>Contact Us</h3>
-                <p><strong>24/7 Phone:</strong><br>(740) 929-4328</p>
-                <p><strong>Address:</strong><br>2070 James Pkwy, Heath<br>OH 43056</p>
+                                <ul class="footer-list">
+                                        <li><a href="<?php echo $base_url ?>/service-area/Heath-Ohio-heating-and-air-conditioning-repair.php">Heath</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Newark-Ohio-heating-and-air-conditioning-repair.php">Newark</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Granville-heating-and-air-conditioning-repair.php">Granville</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Hebron-Ohio-heating-and-air-conditioning-repair.php">Hebron</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Buckeye-Lake-heating-and-air-conditioning-repair.php">Buckeye
+                                                        Lake</a> </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Thornville-Ohio-heating-and-air-conditioning-repair.php">Thornville</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Millersport-heating-and-air-conditioning-repair.php">Millersport</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Pataskala-Ohio-heating-and-air-conditioning-repair.php">Pataskala</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Alexandria-Ohio-heating-and-air-conditioning-repair.php">Alexandria</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Johnstown-Ohio-heating-and-air-conditioning-repair.php">Johnstown</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Glenford-Ohio-heating-and-air-conditioning-repair.php">Glenford</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Kirkersville-Ohio-heating-and-air-conditioning-repair.php">Kirkersville</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Etna-Ohio-heating-and-air-conditioning-repair.php">Etna</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Jacksontown-Ohio-heating-and-air-conditioning-repair.php">Jacksontown
+                                                </a> </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Baltimore-Ohio-heating-and-air-conditioning-repair.php">Baltimore</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Reynoldsburg-Ohio-heating-and-air-conditioning-repair.php">Reynoldsburg</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Canal-Winchester-Ohio-heating-and-air-conditioning-repair.php">Canal
+                                                        Winchester</a> </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/New-Albany-Ohio-heating-and-air-conditioning-repair.php">New
+                                                        Albany</a> </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Pickerington-Ohio-heating-and-air-conditioning-repair.php">Pickerington</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Jersey-Ohio-heating-and-air-conditioning-repair.php">Jersey</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Gratiot-Ohio-heating-and-air-conditioning-repair.php">Gratiot</a>
+                                        </li>
+                                        <li><a href="<?php echo $base_url ?>/service-area/Pleasantville-Ohio-heating-and-air-conditioning-repair.php">Pleasantville</a>
+                                        </li>
+                                </ul>
+                        </div>
+                        <div class="col-md-4 col-sm-12 col-xs-12 footer-item">
+                                <h3>Contact Us</h3>
+                                <p><strong>24/7 Phone:</strong><br>(740) 929-4328</p>
+                                <p><strong>Address:</strong><br>2070 James Pkwy, Heath<br>OH 43056</p>
 
-                <p><a href="<?php echo $base_url ?>/tos-&-pp.php">Terms of Service and Privacy Policy</a></p>
-                <p>
-                <ul class="list-unstyled list-inline">
+                                <p><a href="<?php echo $base_url ?>/tos-&-pp.php">Terms of Service and Privacy
+                                                Policy</a></p>
+                                <p>
+                                <ul class="list-unstyled list-inline">
 
-                    <li><a href="https://www.facebook.com/Performancehcs/" target="_blank"><img
-                                src="<?php echo $base_url ?>/img/facebook.png" style="width: 8%; float: left; margin-right: 10px;"></a>
-                        <a href="https://www.instagram.com/performanceheatingandcooling/"><img
-                                src="<?php echo $base_url ?>/img/instagram.png"
-                                style="width: 8%;float: left;"> </a>
-                    </li>
+                                        <li><a href="https://www.facebook.com/Performancehcs/" target="_blank"><img
+                                                                src="<?php echo $base_url ?>/img/facebook.png"
+                                                                style="width: 8%; float: left; margin-right: 10px;"></a>
+                                                <a href="https://www.instagram.com/performanceheatingandcooling/"><img
+                                                                src="<?php echo $base_url ?>/img/instagram.png"
+                                                                style="width: 8%;float: left;"> </a>
+                                        </li>
 
-                </ul>
-                </p>
+                                </ul>
+                                </p>
 
-            </div>
+                        </div>
 
 
-            </ul>
-        </div>
-        </div>
-    </footer>
+                        </ul>
+                </div>
+                </div>
+        </footer>
 
-    <footer class="full-row mini-footer">
-        <div class="container">
+        <footer class="full-row mini-footer">
+                <div class="container">
 
-            <div class="text-center col-xs-12 footer-item copyrights text-white">
-                <p>All Rights Reserved. Copyrights Performance Heating and Cooling, LLC</p>
-            </div>
+                        <div class="text-center col-xs-12 footer-item copyrights text-white">
+                                <p>All Rights Reserved. Copyrights Performance Heating and Cooling, LLC</p>
+                        </div>
 
-        </div>
-    </footer>
+                </div>
+        </footer>
 </footer>
 
 
@@ -162,32 +146,32 @@
 <script>
 
 
-    jQuery(document).ready(function ($) {
-        var owl = $("#owl-demo");
+        jQuery(document).ready(function ($) {
+                var owl = $("#owl-demo");
 
-        owl.owlCarousel({
-            items: 3, //10 items above 1000px browser width
-            itemsDesktop: [1000, 3], //5 items between 1000px and 901px
-            itemsDesktopSmall: [900, 2], // betweem 900px and 601px
-            itemsTablet: [600, 1], //2 items between 600 and 0
-            itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
-            autoPlay: 3000, //Set AutoPlay to 3 seconds
+                owl.owlCarousel({
+                        items: 3, //10 items above 1000px browser width
+                        itemsDesktop: [1000, 3], //5 items between 1000px and 901px
+                        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+                        itemsTablet: [600, 1], //2 items between 600 and 0
+                        itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+                        autoPlay: 3000, //Set AutoPlay to 3 seconds
+                });
+
+
+                var test = $("#testmonial");
+
+                test.owlCarousel({
+                        items: 1, //10 items above 1000px browser width
+                        itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+                        itemsDesktopSmall: [900, 1], // betweem 900px and 601px
+                        itemsTablet: [600, 1], //2 items between 600 and 0
+                        itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+                        autoPlay: 3000, //Set AutoPlay to 3 seconds
+                });
+
+
         });
-
-
-        var test = $("#testmonial");
-
-        test.owlCarousel({
-            items: 1, //10 items above 1000px browser width
-            itemsDesktop: [1000, 1], //5 items between 1000px and 901px
-            itemsDesktopSmall: [900, 1], // betweem 900px and 601px
-            itemsTablet: [600, 1], //2 items between 600 and 0
-            itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
-            autoPlay: 3000, //Set AutoPlay to 3 seconds
-        });
-
-
-    });
 
 
 </script>
