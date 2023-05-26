babe5fbbe298f19b9df72074417cf84467bc1267
AbubakarCinnova <amahboob@cinnova.com>
2023-05-26 16:46:41 +0500

    update images on installation page

--- installations.php
+++ installations.php
@@ -21,7 +21,7 @@
     Appointment times are incredibly flexible. We prefer that if there is more than one decision maker involved in the process 
     (and there usually is!) that everyone involved be present for the appointment.</p>
 <br><br><br>
-    <p><img src="<?php echo $base_url ?>/img/consulttion.jpg" style="float: right; width: 30%; margin-left: 20px;" alt="image" class="img-responsive" > <strong> 2. Consultation </strong> </p>
+    <p><img src="<?php echo $base_url ?>/img/consultation.jpg" style="float: right; width: 30%; margin-left: 20px;" alt="image" class="img-responsive" > <strong> 2. Consultation </strong> </p>
     
     <p>On your scheduled day, your comfort specialist will arrive and begin by asking you a series of simple questions to get a feel for your needs, your concerns, your budget, and your timeline. After carefully surveying your home and existing HVAC equipment setup, they will usually, in the same visit, be able to present you with a series of options they feel will best fit your needs. The comfort specialists goal is to help you select the system that is a great fit for you and your space. We offer a variety of financing and payment options to help make the purchasing decision easier. Your comfort specialist is fully equipped to assist in securing this financing.</p>
     <br><br>
@@ -29,7 +29,7 @@
     <p><img src="<?php echo $base_url ?>/img/installation-1.jpg" style="float: right; width: 15%; margin-right: 20px;" alt="image" class="img-responsive" >  <strong> 3. Installation </strong> </p>
     <p>Your comfort specialist and installation manager will work with you to schedule a time for your installation. Most installations take no longer than one day; and you will be fully aware of the required schedule. The installing technicians, led by a team leader, will arrive and begin your installation. They will protect their work area and the route to it through your home. They will work to remove any old equipment and then begin placing the new equipment. When they have finished their installation, they will tidy up their work space and the team leader will do a final walk-through, making sure to demonstrating any important features and controls with you.</p>
     <br><br><br><br><br><br>
-    <p><img src="<?php echo $base_url ?>/img/walk-thru.jpg" style="float: right; width: 20%; margin-right: 20px;" alt="image" class="img-responsive" ><strong>4. Post Installation </strong></p>
+    <p><img src="<?php echo $base_url ?>/img/post installation.jpg" style="float: right; width: 20%; margin-right: 20px;" alt="image" class="img-responsive" ><strong>4. Post Installation </strong></p>
     <p>Once your new system has been installed our dedicated staff of back-office personnel will continue working behind the scenes to ensure that the final pieces of your installation come together. They will put together your warranty registration, receipts, and any maintenance agreements and get them sent over to you as quickly as possible.</p><br>
     <p><a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a></p>
 <br/>
